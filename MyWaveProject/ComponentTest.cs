6b44891a8ad413ebb3942c328c9b3344c13ca0cb
Zal0 <gsantos@plainconcepts.com>
2020-01-30 11:26:18 +0100

    First commit

---
+++ MyWaveProject/ComponentTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Input.Keyboard;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Mathematics;
+
+namespace MyWaveProject
+{
+    public class ComponentTest : Behavior
+    {
+        [BindComponent]
+        public Transform3D transform;
+
+        [BindComponent(false,false, BindComponentSource.Scene)]
+        public Camera camera = null;
+
+        public KeyboardDispatcher keyboardDispatcher;
+
+        protected override void OnActivated()
+        {
+            if (camera != null && camera.Display != null)
+            {
+                keyboardDispatcher = camera.Display.KeyboardDispatcher;
+            }
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            float deltaTime = (float)gameTime.TotalSeconds;
+            if (keyboardDispatcher.IsKeyDown(Keys.H))
+            {
+                transform.RotateAround(transform.Position, Vector3.Up, 5.0f * deltaTime);
+            }
+            if (keyboardDispatcher.IsKeyDown(Keys.K))
+            {
+                transform.RotateAround(transform.Position, Vector3.Up, -5.0f * deltaTime);
+            }
+            if (keyboardDispatcher.IsKeyDown(Keys.U))
+            {
+                transform.LocalPosition += transform.LocalOrientation * (Vector3.Backward * 5.0f * deltaTime);
+            }
+            if (keyboardDispatcher.IsKeyDown(Keys.J))
+            {
+                transform.LocalPosition += transform.LocalOrientation * (Vector3.Backward * -5.0f * deltaTime);
+            }
+        }
+    }
+}
